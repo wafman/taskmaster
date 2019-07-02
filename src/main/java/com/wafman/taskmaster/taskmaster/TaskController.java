a53370de042b34594590290ea1db86cbb8e2a4cd
Will Fritts <waf515@gmail.com>
2019-07-02 13:20:28 -0700

    get route working

--- src/main/java/com/wafman/taskmaster/taskmaster/TaskController.java
+++ src/main/java/com/wafman/taskmaster/taskmaster/TaskController.java
@@ -2,16 +2,34 @@ package com.wafman.taskmaster.taskmaster;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
+@RestController
 public class TaskController {
 
     @Autowired
     TaskRespository taskRespository;
 
-//    @GetMapping("/tasks")
-//    public String getTasks(){
+    @GetMapping("/")
+    public String getHome(){
+        return "THIS WORKS";
+    }
+
+    @GetMapping("/tasks")
+    public Iterable<Task> getTasks(){
+        return taskRespository.findAll();
+    }
+
+    @PostMapping("/tasks")
+    public RedirectView createTasks(@RequestParam String title, @RequestParam String description, @RequestParam String status){
+        Task task = new Task(title, description, status);
+        taskRespository.save(task);
+        return new RedirectView("/tasks");
+    }
+
+//    @PutMapping("/tasks/{id}/state")
+//    public String updateTasksState(){
 //
 //    }
 
