15fca6b7c1b87703b0e8615dce510f70745eaf16
Will Fritts <waf515@gmail.com>
2019-07-02 11:23:52 -0700

    initial controller setup

--- src/main/java/com/wafman/taskmaster/taskmaster/TaskController.java
+++ src/main/java/com/wafman/taskmaster/taskmaster/TaskController.java
@@ -1,4 +1,18 @@
 package com.wafman.taskmaster.taskmaster;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TaskController {
+
+    @Autowired
+    TaskRespository taskRespository;
+
+//    @GetMapping("/tasks")
+//    public String getTasks(){
+//
+//    }
+
 }
