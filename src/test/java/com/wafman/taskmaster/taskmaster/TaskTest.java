1a4b77634951f91f2a48678f12e488184060148e
Will Fritts <waf515@gmail.com>
2019-07-02 13:21:43 -0700

    passed integration testing

--- src/test/java/com/wafman/taskmaster/taskmaster/TaskTest.java
+++ src/test/java/com/wafman/taskmaster/taskmaster/TaskTest.java
@@ -1,7 +1,61 @@
 package com.wafman.taskmaster.taskmaster;
 
-import static org.junit.Assert.*;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.model.CreateTableRequest;
+import com.amazonaws.services.dynamodbv2.model.ProvisionedThroughput;
+import javafx.application.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = TaskmasterApplication.class)
+@WebAppConfiguration
+@ActiveProfiles("local")
 public class TaskTest {
+    //integration testing
 
+    private DynamoDBMapper dynamoDBMapper;
+
+    @Autowired
+    private AmazonDynamoDB amazonDynamoDB;
+
+    @Autowired
+    TaskRespository taskRespository;
+
+    private static final String EXPECTED_TITLE = "Test Title";
+    private static final String EXPECTED_DESCRIPTION = "Test Description";
+    private static final String EXPECTED_STATUS = "Test Status";
+
+    @Before
+    public void setup() throws Exception {
+        dynamoDBMapper = new DynamoDBMapper(amazonDynamoDB);
+
+        CreateTableRequest tableRequest = dynamoDBMapper.generateCreateTableRequest(Task.class);
+
+        tableRequest.setProvisionedThroughput(new ProvisionedThroughput(1L, 1L));
+
+        dynamoDBMapper.batchDelete((List<Task>)taskRespository.findAll());
+    }
+
+    @Test
+    public void readWriteTestCase() {
+        Task task = new Task(EXPECTED_TITLE, EXPECTED_DESCRIPTION, EXPECTED_STATUS);
+        taskRespository.save(task);
+
+        List<Task> result = (List<Task>) taskRespository.findAll();
+
+        assertTrue("Not empty", result.size() > 0);
+        assertTrue("Contains item with expected cost", result.get(0).getTitle().equals(EXPECTED_TITLE));
+    }
 }
