5cb8b5687a235e86861f44ff0349c23654c4aaca
Will Fritts <waf515@gmail.com>
2019-07-02 11:24:10 -0700

    set up table respository

--- src/main/java/com/wafman/taskmaster/taskmaster/TaskRespository.java
+++ src/main/java/com/wafman/taskmaster/taskmaster/TaskRespository.java
@@ -1,4 +1,11 @@
 package com.wafman.taskmaster.taskmaster;
 
-public interface TaskRespository {
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+@EnableScan
+public interface TaskRespository extends CrudRepository<Task, Long> {
+    Optional<Task> findById(String id);
 }
